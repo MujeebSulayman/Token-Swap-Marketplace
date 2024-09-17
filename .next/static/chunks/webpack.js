29f71ecc364619c09ca354b15a8815b343301ea5
TheHemjay <79232253+MujeebSulayman@users.noreply.github.com>
2024-09-18 00:47:27 +0100

    TokenBalance

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "370cf8246245cd9f"; }
+/******/ 		__webpack_require__.h = function() { return "b341fd8aac7fccc1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/tokens.js
+++ pages/tokens.js
@@ -14,7 +14,6 @@ const tokens = () => {
 		loadData();
 	}, []);
 
-	console.log(history);
 	return (
 		<>
 			<Header />
